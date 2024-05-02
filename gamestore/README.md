a06440f2bb0c897ba2a522f2c56c6fc8de861a21
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-02 15:27:43 -0300

    att

---
+++ gamestore/README.md
@@ -0,0 +1,13 @@
+- Model
+    - Game
+- Game Controller
+    - Listar os games
+    - Cadastrar novo Game
+- Game Repository
+    - CRUD
+- IndexController
+    - Rotas de Navegação
+- Template
+    -gameadd.html
+    -gamelist.html
+    
---
+++ gamestore/src/main/java/br/com/theuzcoder/gamestore/Model/GameModel.java
@@ -0,0 +1,55 @@
+package br.com.theuzcoder.gamestore.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class GameModel {
+    //atributos
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String nome;
+    String descricao;
+    String desenvolvedora;
+    double preco;
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDesenvolvedora() {
+        return desenvolvedora;
+    }
+    public void setDesenvolvedora(String desenvolvedora) {
+        this.desenvolvedora = desenvolvedora;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    
+}
